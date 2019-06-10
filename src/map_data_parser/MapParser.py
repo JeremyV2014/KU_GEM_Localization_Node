0d49190145d8d3276bdc3b477f3703604ab57e27
ashea2120 <ashea2120@yahoo.com>
2019-06-10 14:07:05 -0400

    updated csv filepaths to the raspberry pi directories

--- src/map_data_parser/MapParser.py
+++ src/map_data_parser/MapParser.py
@@ -1,6 +1,11 @@
 import csv
 
-with open('FILEPATH\\MRC_Cut_Middle.csv') as csv_file:
+#added in all the different HD maps that are available to us at this time 
+#with open('//home//pi//HDmaps//MRC_Loop_1_Middle.csv') as csv_file:
+#with open('//home//pi//HDmaps//MRC_Pad_Edge.csv') as csv_file:
+#with open('//home//pi//HDmaps//MOuter_Loop_Middle.csv') as csv_file:
+#with open('//home//pi//HDmaps//MRC_ShortCut_Middle.csv') as csv_file:
+with open('//home//pi//HDmaps//MRC_Cut_Middle.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
 
@@ -27,5 +32,6 @@ def calculateLatLongPair(latitude, longtitude, latlonglist):
     returnedPair = calculateLatLongPair(43.012045,-83.708319, lat_lng)
     #print(returnedPair)
 
+#Doesn't do anything else with the data at this moment other than printing
 print(lat_lng)
 print(lat_lng[0])
